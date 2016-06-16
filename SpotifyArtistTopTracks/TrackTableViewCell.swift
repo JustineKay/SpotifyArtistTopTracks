bcdae20f17502683613a7ef13b07b0c3e289d8c3
Justine Kay <justinekay@Justines-MacBook-Pro.local>
2016-06-16 10:38:57 -0400

    Set up custom track table view cell

---
+++ SpotifyArtistTopTracks/TrackTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrackTableViewCell.swift
+//  SpotifyArtistTopTracks
+//
+//  Created by Justine Kay on 6/16/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+import UIKit
+
+class TrackTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
