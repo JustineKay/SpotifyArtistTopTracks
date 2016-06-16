4b4e6bd9297168a0bbd0dc7ff5b0e3b0e33fd291
Justine Kay <justinekay@Justines-MacBook-Pro.local>
2016-06-16 14:54:50 -0400

    Custom track tableViewCell loading successfully

--- SpotifyArtistTopTracks/SATopTracksTableViewController.swift
+++ SpotifyArtistTopTracks/SATopTracksTableViewController.swift
@@ -14,8 +14,8 @@ class SATopTracksTableViewController: UITableViewController {
     @IBOutlet weak var artistNameLabel: UILabel!
     
     var spotifyArtist = SpotifyArtist()
-    var results = [SpotifyArtist.Track()]
-    let trackCellReuseIdentifier = "trackCellReuseIdentifier"
+    var results = [Track]()
+    let trackCellReuseIdentifier = "TrackTableViewCellIdentifier"
     
     override func viewDidLoad()
     {
@@ -23,6 +23,23 @@ class SATopTracksTableViewController: UITableViewController {
         fetchArtistTopTracks()
         self.tableView.backgroundColor = UIColor.blackColor()
         artistNameLabel.text = spotifyArtist.name
+        
+        setUpCustomTableViewCell()
+        autoAdjustTableViewRowHeight()
+    }
+    
+    //MARK: - UI
+    
+    func setUpCustomTableViewCell()
+    {
+        let nib = UINib.init(nibName: "TrackTableViewCell", bundle: nil)
+        self.tableView.registerNib(nib, forCellReuseIdentifier: trackCellReuseIdentifier)
+    }
+    
+    func autoAdjustTableViewRowHeight()
+    {
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 44.0
     }
     
     //MARK: - Network request
@@ -55,15 +72,15 @@ class SATopTracksTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCellWithIdentifier(trackCellReuseIdentifier, forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(trackCellReuseIdentifier, forIndexPath: indexPath) as? TrackTableViewCell
         
         let track = results[indexPath.row]
-        cell.textLabel?.text = track.name
-        cell.textLabel?.textColor = UIColor.init(colorLiteralRed: 230.0/255, green: 230.0/255, blue: 230.0/255, alpha: 1)
-        cell.textLabel?.font = UIFont.init(name: "Montserrat", size: 17.0)
-        cell.backgroundColor = UIColor.blackColor()
+        cell!.trackNameLabel.text = track.name
+//        cell.textLabel?.textColor = UIColor.init(colorLiteralRed: 230.0/255, green: 230.0/255, blue: 230.0/255, alpha: 1)
+//        cell.textLabel?.font = UIFont.init(name: "Montserrat", size: 17.0)
+//        cell.backgroundColor = UIColor.blackColor()
 
-        return cell
+        return cell!
     }
     
     @IBAction func popVC(sender: UIButton)
